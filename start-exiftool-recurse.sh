2cd3bac1fd8c46903d69f6eeb3e7af588bae4433
nvanderperren <ik.ben.nas@gmail.com>
2019-03-06 14:26:35 +0000

    add exiftool for image files spreat out ove various folders

---
+++ start-exiftool-recurse.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# author: Nastasia Vanderperren (PACKED/VIAA)
+# goal: use exiftool to recursive write out 
+# exif metadata of images in a csv file exiftool.csv
+# it searches recursive to files with mimetype jpeg and tif
+# requires: exiftool to be installed
+# use sudo apt install libimage-exiftool-perl
+#
+
+
+folder=$1
+outputfolder=$2
+
+exiftool -recurse -csv -ext jpg -ext jepg -ext tif -ext tiff $folder > $outputfolder/exiftool.csv
+
