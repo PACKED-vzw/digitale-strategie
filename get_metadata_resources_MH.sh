eca5ed3172455ba8576dcef8ae9fdada04b124ac
nvanderperren <ik.ben.nas@gmail.com>
2019-07-22 14:19:52 +0200

    working script for scraping metadata Mediahaven

--- get_metadata_resources_MH.sh
+++ get_metadata_resources_MH.sh
@@ -1,32 +1,74 @@
 #!/bin/bash
 
-organisation=$1
-aantal=$2
-server=Mediahaven
-header_database=filename,filesize,modified,md5,format,mime,server,organisation
-header_download=fragment_id,filename,extension
+token=$1
+organisation=$2
+invoerruimte=$3
+server="Mediahaven"
+header_database="filename,filesize,modified,md5,format,mime,server,organisation"
+header_download="fragment_id,filename,extension"
+csv_database="$organisation"_database_MH.csv
+csv_download="$organisation"_download_MH.csv
 
-
-# idea --> iterate over complex object and save them as seperate json objects
-# do then stuff with the json document
-# delete then the them json folder
-# arithmic with bash variable, e.g. $((aantal-1))
-
-
-for i in $aantal; do {
-
+function getFileType {
+    extension=$1
+    case $extension in 
+        jpg)
+            filetype="Raw JPEG Stream"
+        ;;
+        tif)
+            filetype="Exchangeable Image File Format (Uncompressed)"
+        ;;
+    esac
+    echo $filetype
 }
 
-for json in $(jq -r '.MediaDataList[].Internal')
+function getAantal {
+    aantal=$(curl -s -X GET \
+    -H "Authorization: Bearer ${token}" \
+    -H 'Accept: application/vnd.mediahaven.v2+json' \
+    "https://hasselt.debeeldbank.be/mediahaven-rest-api/resources/media/?q=%2B(IngestSpaceId:%22${invoerruimte}%22)%20%2B(IsInIngestSpace%3A1)&nrOfResults=1" \
+    | jq .TotalNrOfResults)
+    echo $aantal
+}
 
+function doCallMediaHaven {
+    aantal=$1
+    curl -s -X GET \
+    -H "Authorization: Bearer ${token}" \
+    -H 'Accept: application/vnd.mediahaven.v2+json' \
+    "https://hasselt.debeeldbank.be/mediahaven-rest-api/resources/media/?q=%2B(IngestSpaceId:%22${invoerruimte}%22)%20%2B(IsInIngestSpace%3A1)&nrOfResults=${aantal}" \
+    | jq .MediaDataList > "$organisation"_MH.json
 
-fragment_id=$(jq '.MediaDataList[].Internal.FragmentId' $json)
-extension=$(jq '.MediaDataList[].Technical.OriginalExtension' $json)
+    echo "$organisation"_MH.json
+}
 
+function createCSVs {
+echo $header_database > "$csv_database"
+echo $header_download > "$csv_download"
+echo $organisation
 
-filename=$(jq '.MediaDataList[].Descriptive.OriginalFilename' $json)
-filesize=$(jq '.MediaDataList[].Technical.FileSize' $json)
-modified=(jq '.MediaDataList[].Administrative.LastModifiedDate' $json)
-md5=$(jq '.MediaDataList[].Technical.Md5' $json)
-mime=$(jq '.MediaDataList[].Technical.MimeType' $json)
+    aantal=$(getAantal)
+    echo $aantal
+    aantal=10
 
+    json=$(doCallMediaHaven $aantal)
+
+    for i in $(seq 1 $((aantal-1))) 
+    do 
+        fragment_id=$(jq -r .[$i].Internal.FragmentId $json)
+        extension=$(jq -r .[$i].Technical.OriginalExtension $json)
+        filename=$(jq .[$i].Descriptive.OriginalFilename $json)
+        filesize=$(jq .[$i].Technical.FileSize $json)
+        format=$(getFileType $extension)
+        modified=$(jq .[$i].Administrative.LastModifiedDate $json)
+        md5=$(jq .[$i].Technical.Md5 $json)
+        mime=$(jq .[$i].Technical.MimeType $json)
+
+        echo $fragment_id,$filename,$extension >> "$csv_download"
+        echo $filename,$filesize,$modified,$md5,$format,$mime,$server,$organisation >> "$csv_database"
+    done
+    echo "finished! done done done"
+}
+
+## main script
+createCSVs
